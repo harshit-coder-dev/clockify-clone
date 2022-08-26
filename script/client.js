a331b7ac58d344686de792a25d82699070c4315b
282Manoj <manojyad436@gamil.com>
2022-08-26 13:32:36 +0530

    file added

--- script/client.js
+++ script/client.js
@@ -1,48 +1,115 @@
-// window.addEventListener("load", () => {
-    let clientArr = JSON.parse(localStorage.getItem("client")) || [];
-    display(clientArr);
-// })
+import {navegabar , SideBar } from "../component/navSide.js";
 
 
-let add = document.getElementById("add");
-add.addEventListener("click", () => {
-    let clientArr = JSON.parse(localStorage.getItem("client")) || [];
-    let value = document.getElementById("add-client").value;
-    clientArr.unshift(value);
-    localStorage.setItem("client", JSON.stringify(clientArr));
-    display(clientArr);
+
+
+document.getElementById("navbar").innerHTML = navegabar();
+let sidebar = document.getElementById("sidebar");
+sidebar.innerHTML = SideBar();
+
+
+
+
+var menu = document.getElementById("menu-icon");
+menu.onclick = () => {
+    myFunction();
+}
+function myFunction() {
+    let x = document.getElementsByClassName("hide-show");
+    for (let i = 0; i < x.length; i++) {
+
+        if (x[i].style.display === "none") {
+            x[i].style.display = "block";
+            sidebar.style.width = "200px";
+
+        }
+        else {
+            x[i].style.display = "none";
+            sidebar.style.width = "50px";
+
+        }
+    }
+}
+// window.onload = myFunction();
+const one = document.querySelector("#one");
+
+one.addEventListener("click", () => {
+    let show = document.getElementById("show");
+    const hide = document.querySelector("#more")
+    hide.classList.toggle("hide1");
+    // console.log(show.innerText);
+    if (show.innerText == "Show More") {
+        show.innerText = "Show Less"
+        one.classList.toggle("rotate");
+
+    }
+    else {
+        show.innerText = "Show More";
+        one.classList.toggle("rotate");
+    }
+
 })
 
 
 
 
-let display = ( clientArr,) => {
-    console.log(clientArr);
-    let cont = document.getElementById("container");
+const manoj = document.getElementById("user-name");
+const drop = document.querySelector(".dropdown");
+manoj.addEventListener("click", () => {
+    if (drop.style.display === "none") {
+        drop.style.display = "block";
+    }
+    else {
+        drop.style.display = "none";
+    }
+})
+const ques = document.getElementById("question");
+const drop2 = document.querySelector(".dropdown2");
+ques.addEventListener("click", () => {
+    if (drop2.style.display === "none") {
+        drop2.style.display = "block";
+    }
+    else {
+        drop2.style.display = "none";
+    }
+})
+const noti = document.getElementById("notification");
+const drop3 = document.querySelector(".dropdown3");
+noti.addEventListener("click", () => {
+    if (drop3.style.display === "none") {
+        drop3.style.display = "block";
+    }
+    else {
+        drop3.style.display = "none";
+    }
+})
+const user = document.getElementById("name-short");
+const drop4 = document.querySelector(".dropdown4");
+user.addEventListener("click", () => {
+    if (drop4.style.display === "none") {
+        drop4.style.display = "block";
+    }
+    else {
+        drop4.style.display = "none";
+    }
+})
 
-    console.log(cont);
-    cont.innerHTML = null;
-    clientArr.forEach(el => {
-        let div = document.createElement("div");
-        div.setAttribute("class", "show-div");
-        let client = document.createElement("p");
+const check = document.getElementById("check");
+check.addEventListener("change", () => {
+    document.body.classList.toggle("darkTheme");
 
-        client.innerText = el;
-        let div2 = document.createElement("div");
-         div2.setAttribute("class","right-div");
-        let div3 = document.createElement("div");
+})
 
-        let div4 = document.createElement("div");
 
-        let img1 = document.createElement("img");
-        img1.src = "https://app.clockify.me/assets/ui-icons/edit.svg";
-        div3.append(img1)
-        let img2 = document.createElement("img");
-        img2.src = "https://app.clockify.me/assets/ui-icons/menu-dots-vertical.svg";
-        div4.append(img2)
-        div2.append(div3, div4);
-        div.append(client, div2);
 
-        cont.append(div);
-    });
-}
+let clt=document.getElementById("clt");
+console.log(clt);
+clt.addEventListener("click",()=>{
+window.location.href="client.html"
+})
+
+
+
+
+
+
