eb3d0b5045def3a002500a7af4d39af7dcce49e9
Rup1999 <rup.bhunia1999@gmail.com>
2022-08-28 00:11:36 +0530

    project done

--- script/timetracker.js
+++ script/timetracker.js
@@ -1,29 +1,4 @@
 
-<<<<<<< HEAD
-let [seconds,minutes,hours] = [0,0,0];
-let timerRef = document.querySelector('#timer>p');
-let int = null;
-let btn=document.getElementById('timer_start')
-let c=0;
-btn.addEventListener('click', ()=>{
-    c++;
-    if(c%2==1){
-        btn.innerText="Stop";
-        btn.style.background="red";
-        btn.style.color="white";
-        int = setInterval(displayTimer,60);
-    }else{
-        btn.innerText="Start";
-        btn.style.background="#5FBDF7";
-        btn.style.color="white";
-        clearInterval(int);
-    }
- 
-});
-
-// btn.addEventListener('click', ()=>{
-    
-=======
 let [seconds, minutes, hours] = [0, 0, 0];
 let timerRef = document.querySelector('#timer>p');
 let int = null;
@@ -48,7 +23,6 @@ btn.addEventListener('click', () => {
 
 // btn.addEventListener('click', ()=>{
 
->>>>>>> 52e5a8df881c8634a87e3371d96e399209bffb42
 // });
 
 // document.getElementById('resetTimer').addEventListener('click', ()=>{
@@ -57,34 +31,17 @@ btn.addEventListener('click', () => {
 //     timerRef.innerHTML = '00 : 00 : 00 : 000 ';
 // });
 
-<<<<<<< HEAD
-function displayTimer(){
-    seconds+=1;
-    if(seconds == 60){
-        seconds = 0;
-        minutes++;
-        if(minutes == 60){
-=======
 function displayTimer() {
     seconds += 1;
     if (seconds == 60) {
         seconds = 0;
         minutes++;
         if (minutes == 60) {
->>>>>>> 52e5a8df881c8634a87e3371d96e399209bffb42
             minutes = 0;
             hours++;
         }
     }
 
-<<<<<<< HEAD
- let h = hours < 10 ? "0" + hours : hours;
- let m = minutes < 10 ? "0" + minutes : minutes;
- let s = seconds < 10 ? "0" + seconds : seconds;
-
-
- timerRef.innerHTML = ` ${h} : ${m} : ${s}`;
-=======
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
@@ -167,25 +124,13 @@ function showAppend() {
     div.append(div1, div2);
     app.append(div)
 
->>>>>>> 52e5a8df881c8634a87e3371d96e399209bffb42
 }
 
 
 
 
-<<<<<<< HEAD
-let Close=document.querySelector(".cls-new-prj")
-let Create=document.querySelector(".crt-new-prj");
-
-let modal=document.getElementById("modal");
-let pro=document.querySelector(".project");
-pro.addEventListener("click",()=>{
-    openModal()
-})
-=======
 
 
->>>>>>> 52e5a8df881c8634a87e3371d96e399209bffb42
 
 
 
